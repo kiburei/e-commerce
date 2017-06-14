4fde140dd5ef8d6da769427791f3b771e6ebf201
kiburei <akiburei@gmail.com>
2017-06-14 12:39:30 +0300

    Add data products to model

--- app/models/products.js
+++ app/models/products.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+    name: DS.attr(),
+    price: DS.attr(),
+    description: DS.attr(),
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
+    var products = [{
+                name: 'Meko',
+                price: "KES 12,000",
+                description: "On Fire, woun't go off",
+                image: ""
+                }, {
+                name: 'Crib',
+                price: "KES 15,500",
+                description: "That kid is not going any where",
+                image: ""
+                }, {
+                name: 'Percolator',
+                price: "KES 7,000",
+                description: "Heated the water of life",
+                image: ""
+}];
 
 export default Ember.Route.extend({
+    model () {
+        return products;
+    }
 });
